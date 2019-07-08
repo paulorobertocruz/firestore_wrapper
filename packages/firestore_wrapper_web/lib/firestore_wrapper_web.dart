3a46293a88e5c7bdae81dde12ffbabd3f7eccf66
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-07-07 23:17:58 -0300

    adiciona implementação inicial do firestore wrapper web

---
+++ packages/firestore_wrapper_web/lib/firestore_wrapper_web.dart
@@ -0,0 +1,8 @@
+part 'package:firestore_wrapper_web/src/firestore.dart';
+part 'package:firestore_wrapper_web/src/collection_reference.dart';
+part 'package:firestore_wrapper_web/src/document_reference.dart';
+part 'package:firestore_wrapper_web/src/document_snapshot.dart';
+part 'package:firestore_wrapper_web/src/query.dart';
+part 'package:firestore_wrapper_web/src/query_snapshot.dart';
+
+
