64e5ae267f48b509ee9b8b70577dcd3261d39036
catalunha <catalunha.mj@gmail.com>
2019-07-24 11:03:56 -0300

    adicionar field value

--- packages/firestore_wrapper_flutter/lib/firestore_wrapper_flutter.dart
+++ packages/firestore_wrapper_flutter/lib/firestore_wrapper_flutter.dart
@@ -4,3 +4,4 @@ export 'package:firestore_wrapper_flutter/src/document_reference.dart';
 export 'package:firestore_wrapper_flutter/src/document_snapshot.dart';
 export 'package:firestore_wrapper_flutter/src/query.dart';
 export 'package:firestore_wrapper_flutter/src/query_snapshot.dart';
+export 'package:firestore_wrapper_flutter/src/field_value.dart';
