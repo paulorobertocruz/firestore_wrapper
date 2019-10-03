324a7f9113e5caf35dc8df3a44ade9c9bf4e7a53
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-10-03 10:10:35 -0300

    arrayContains para query.where no mobile

--- packages/firestore_wrapper_flutter/lib/src/collection_reference.dart
+++ packages/firestore_wrapper_flutter/lib/src/collection_reference.dart
@@ -79,6 +79,7 @@ class CollectionReference extends fw.CollectionReference {
           isGreaterThan: isGreaterThan,
           isGreaterThanOrEqualTo: isGreaterThanOrEqualTo,
           isLessThanOrEqualTo: isLessThanOrEqualTo,
+          arrayContains: arrayContains,
           isNull: isNull,
         ));
   }
