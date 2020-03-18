516971c2896fb719f88da368b780dd34453a2393
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2020-03-18 14:33:11 -0300

    implementa collectionGroup no flutter

--- packages/firestore_wrapper_flutter/lib/src/firestore.dart
+++ packages/firestore_wrapper_flutter/lib/src/firestore.dart
@@ -2,13 +2,19 @@ import 'package:firestore_wrapper/firestore_wrapper.dart' as fw;
 import 'package:cloud_firestore/cloud_firestore.dart' as fs;
 import 'package:firestore_wrapper_flutter/src/collection_reference.dart';
 import 'package:firestore_wrapper_flutter/src/document_reference.dart';
+import 'package:firestore_wrapper_flutter/src/query.dart';
 
-class Firestore extends fw.Firestore{
-    final fs.Firestore _firestore = fs.Firestore.instance;
+class Firestore extends fw.Firestore {
+  final fs.Firestore _firestore = fs.Firestore.instance;
 
   @override
   CollectionReference collection(String path) {
     return CollectionReference(this, _firestore.collection(path));
+  }
+
+  @override
+  Query collectionGroup(String path) {
+    return Query(this, _firestore.collectionGroup(path));
   }
 
   @override
