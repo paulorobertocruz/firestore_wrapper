07efb158c570765e78fb56329bed9297900b976f
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2020-03-18 14:32:36 -0300

    cria definição do metodo collectionGroup na classe abstrata Firestore

--- packages/firestore_wrapper/lib/src/firestore.dart
+++ packages/firestore_wrapper/lib/src/firestore.dart
@@ -2,5 +2,6 @@ part of 'package:firestore_wrapper/firestore_wrapper.dart';
 
 abstract class Firestore{
   CollectionReference collection(String path);
+  Query collectionGroup(String path);
   DocumentReference document(String path);
 }
